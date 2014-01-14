a72aed8f55a7bc4c0df66bf4e10b3fb4f3d7a8e0
Michael Mattheakis <michaelm244@gmail.com>
2014-01-14 15:03:43 -0800

    fixed a lot of things

--- background.js
+++ background.js
@@ -1,35 +1,39 @@
-var counter = 0;
 var url = chrome.extension.getURL("man.jpg");
-var insert = "<img style=\"display:inline;width:20px;height:20px;\" src=\""+url+"\">";
-var jump1 = insert.length - "slash".length;
-var jump2 = insert.length - "/".length;
-var bodytag = $("body")[0];
-var html = bodytag.innerHTML;
-var inAttr = false;
-for(var j = 0; j < html.length; j++) {
-	if(html.charAt(j) == "<") {
-		// start of tag
-		inAttr = true;
+(function(){
+	changeHTML(document.body);
+})();
+function changeHTML(tag) {
+	var inner = false;
+	if(tag.childNodes.length == 0)
+		inner = true;
+	if(inner) {
+		if(tag.constructor.name == "Text") {
+			// use nodeValue to replace content
+			var newTag = document.createElement("span");
+			var text = tag.nodeValue;
+			text = text.replace(/\//g, "<img style=\"display:inline;width:20px;height:20px;\" src=\""+url+"\">");
+			text = text.replace(/slash/gi, "<img style=\"display:inline;width:20px;height:20px;\" src=\""+url+"\">");
+			newTag.innerHTML = text;
+			tag.parentNode.insertBefore(newTag, tag);
+			tag.remove();
+		}
+		else if(tag.constructor.name != "Comment") {
+			// else, use innerHTML to replace content
+			try {
+			tag.innerHTML = tag.innerHTML.replace(/\//g, "<img style=\"display:inline;width:20px;height:20px;\" src=\""+url+"\">");
+			tag.innerHTML = tag.innerHTML.replace(/slash/gi, "<img style=\"display:inline;width:20px;height:20px;\" src=\""+url+"\">");
+			}
+			catch(err) {
+				console.log("error! tag with error is below:");
+				console.log(tag);
+				console.log(tag.constructor.name);
+				for(prop in tag){console.log(prop);}
+			}
+		}
 	}
-	else if(html.charAt(j) == ">") {
-		// close of tag
-		inAttr = false;
-	}
-	else if(!inAttr && j+4<html.length && html.substring(j,j+5).toLowerCase() == "slash") {
-		// this is if were not in a tag, then replace
-		var begin = html.substring(0,j);
-		var end = html.substring(j+5);
-		html = begin+insert+end;
-		j+=jump1;
-		counter++;
-	}
-	else if(!inAttr && html.charAt(j) == "/") {
-		// this is if were not in a tag, then replace
-		var begin = html.substring(0,j);
-		var end = html.substring(j+1);
-		html = begin+insert+end;
-		j+=jump2;
-		counter++;
+	else {
+		for(var i = 0; i < tag.childNodes.length; i++) {
+			changeHTML(tag.childNodes[i]);
+		}
 	}
 }
-bodytag.innerHTML = html;
